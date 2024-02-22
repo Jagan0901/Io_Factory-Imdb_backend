0dc14113d9764da5c89f89839ad3a4f6e92116db
Jagan0901 <110884222+Jagan0901@users.noreply.github.com>
2024-02-22 18:12:25 +0530

    init commit

---
+++ helper.js
@@ -0,0 +1,100 @@
+import { client } from "./index.js";
+import bcrypt from "bcrypt";
+
+//Movies
+export async function getMovies(req) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("movies")
+    .find(req.query)
+    .toArray();
+}
+
+export async function addMovies(newMovie) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("movies")
+    .insertOne(newMovie);
+}
+
+export async function getMoviesById(movieId) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("movies")
+    .findOne({ id: movieId });
+}
+
+export async function deleteMoviesbyId(movieId) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("movies")
+    .deleteOne({ id: movieId });
+}
+
+export async function editMovieById(movieId, updatedMovie) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("movies")
+    .updateOne({ id: movieId }, { $set: updatedMovie });
+}
+
+//TVShows
+export async function getShows(req) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("shows")
+    .find(req.query)
+    .toArray();
+}
+
+export async function addShows(newShow) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("shows")
+    .insertOne(newShow);
+}
+
+export async function getShowsById(showId) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("shows")
+    .findOne({ id: showId });
+}
+
+export async function deleteShowsbyId(showId) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("shows")
+    .deleteOne({ id: showId });
+}
+
+export async function editShowById(showId, updatedShow) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("shows")
+    .updateOne({ id: showId }, { $set: updatedShow });
+}
+
+//users
+export async function genPassword(password) {
+  const salt = await bcrypt.genSalt(10); //bcrypt.genSalt(no of rounds)
+  console.log(salt);
+  const hashedPassword = await bcrypt.hash(password, salt);
+  console.log(hashedPassword);
+  return hashedPassword;
+}
+// console.log(genPassword("password"));
+
+export async function createUser(username, hashedPassword) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("users")
+    .insertOne({ username: username, password: hashedPassword });
+}
+
+export async function getUserByName(username) {
+  return await client
+    .db("IoFactory-Imdb")
+    .collection("users")
+    .findOne({ username: username });
+}
